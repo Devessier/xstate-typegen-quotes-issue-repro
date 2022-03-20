52e49a25d53f65009da7cd81ae1b3ff8a202e35a
Devessier <baptiste@devessier.fr>
2022-03-20 11:59:32 +0100

    feat: create two machines with typegen

---
+++ valid-machine.ts
@@ -0,0 +1,20 @@
+import { createMachine } from "xstate";
+
+createMachine({
+  id: "Machine",
+  tsTypes: {} as import("./valid-machine.typegen").Typegen0,
+  schema: {
+    events: {} as { type: "Event" },
+  },
+  initial: "State 1",
+  states: {
+    "State 1": {
+      on: {
+          Event: {
+              target: 'State 2'
+          }
+      },
+    },
+    "State 2": {},
+  },
+});
